a0d2d588c78b76e9dce2ffbe720b8e9653c1d9cb
Kelvin <keljr021@gmail.com>
2025-12-13 18:53:17 -0500

    Added vue-router

---
+++ src/components/Navbar.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+  <div style="background:#ccc">
+    Navbar
+  </div>
+</template>
---
+++ src/pages/About.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+  <div>About view</div>
+</template>
+
+<style scoped>
+</style>
