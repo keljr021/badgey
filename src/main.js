ab32f3696765ce980df023dc2f15741e442992e0
Kelvin <keljr021@gmail.com>
2025-12-13 18:29:18 -0500

    Refactored with Vite and packages

--- src/main.js
+++ src/main.js
@@ -1,11 +1,5 @@
 import { createApp } from 'vue'
+import './style.css'
 import App from './App.vue'
-import router from './router'
-import ui from '@nuxt/ui/vue-plugin'
 
-const app = createApp(App)
-
-app.use(ui)
-app.use(router)
-
-app.mount('#app')
+createApp(App).mount('#app')
