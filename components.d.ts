1d2294766800e3d8a9fd685dc02f4d1bbacd0e85
Kelvin <keljr021@gmail.com>
2025-12-23 19:31:06 -0500

    Added timeline to release notes

--- components.d.ts
+++ components.d.ts
@@ -38,5 +38,6 @@ declare module 'vue' {
     USelect: typeof import('./node_modules/@nuxt/ui/dist/runtime/components/Select.vue')['default']
     UTabs: typeof import('./node_modules/@nuxt/ui/dist/runtime/components/Tabs.vue')['default']
     UTextarea: typeof import('./node_modules/@nuxt/ui/dist/runtime/components/Textarea.vue')['default']
+    UTimeline: typeof import('./node_modules/@nuxt/ui/dist/runtime/components/Timeline.vue')['default']
   }
 }
