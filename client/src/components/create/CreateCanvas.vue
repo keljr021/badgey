05696dd8b94683380190053dab7a66d11420b9e7
Kelvin <keljr021@gmail.com>
2026-08-10 18:13:00 -0400

    Adjusted width and background of canvas

--- client/src/components/create/CreateCanvas.vue
+++ client/src/components/create/CreateCanvas.vue
@@ -1,9 +1,21 @@
 <script setup>
+import { reactive } from 'vue'
+
+const konvaConfig = reactive({
+  width: 400,
+  height: 400,
+  blue: 100,
+});
 </script>
 
 <template>
   <div class="canvas">
-    Canvas here
+    <div class="canvas-container">
+      <v-stage :config="konvaConfig" style="width: 100%; height: 100%; background-color: #eee;">
+        <v-layer>
+        </v-layer>
+      </v-stage>
+    </div>
   </div>
 </template>
 
@@ -11,7 +23,12 @@
 .canvas {
   width: 100%;
   height: 100%;
-  background-color: #d9d9d9;
   padding: 10px;
+}
+
+.canvas-container {
+  width: 50%;
+  height: 100%;
+  margin: 0 auto;
 }
 </style>
--- client/src/components/create/partials/CreateMenuImport.vue
+++ client/src/components/create/partials/CreateMenuImport.vue
@@ -12,7 +12,7 @@
     position: absolute;
     top: 420px;
     left: 100px;
-    min-width: 550px;
+    min-width: 350px;
     min-height: 100px;
     background-color: #fff;
     box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25);
