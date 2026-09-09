f2069581be4222145a0ea12352c6fb0fc41ef101
Kelvin <keljr021@gmail.com>
2026-09-08 22:40:30 -0400

    Update CreateSave.vue

--- client/src/components/create/CreateSave.vue
+++ client/src/components/create/CreateSave.vue
@@ -27,7 +27,7 @@ const publishBadge = () => {
 <template>
   <div class="save-buttons">
     <UButton @click="reset" class="mx-2 cursor-pointer" icon="i-lucide-trash-2" label="Reset" color="neutral" variant="outline" />
-    <UButton @click="saveDraft" class="mx-2 cursor-pointer" icon="i-lucide-file-plus" label="Save as draft" color="neutral" variant="outline" />
+    <UButton @click="saveDraft" class="mx-2 cursor-pointer" icon="i-lucide-file-plus-2" label="Save as draft" color="neutral" variant="outline" />
     <UButton @click="publishBadge" class="mx-2 cursor-pointer" icon="i-lucide-upload" label="Publish badge" color="neutral" variant="outline" />
   </div>
 </template>
