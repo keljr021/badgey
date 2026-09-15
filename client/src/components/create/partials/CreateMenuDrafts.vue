8a35973ed4ef9f90fa82d0e55baa0a8d98f35f77
Kelvin <keljr021@gmail.com>
2026-09-14 22:12:08 -0400

    Scaffholded new open and delete methods for drafts

--- client/src/components/create/partials/CreateMenuDrafts.vue
+++ client/src/components/create/partials/CreateMenuDrafts.vue
@@ -13,25 +13,35 @@ const { drafts } = storeToRefs(draftStore);
 const selectedDraft = ref(null);
 const searchTerm = ref('');
 
-const openDraft = id => {
-    console.log('open draft: ', id);
+const closeMenu = () => {
+  selectedDraft.value = null;
+  emit('close');
 }
 
-watch(selectedDraft, (newValue) => {
-  const id = newValue.value;
-  console.log('Selected draft: ', id);
-  openDraft(id);
-});
+const openDraft = () => {
+    console.log('open draft: ', selectedDraft.value.value);
+}
+
+const deleteDraft = () => {
+  const confirmDelete = confirm('Are you sure you want to this draft: ' + selectedDraft.value.value + '?');
+  if (confirmDelete) {
+      console.log('delete draft: ', selectedDraft.value.value);
+  }
+} 
 </script>
 
 <template>
   <div class="create-menu-drafts">
     <div class="create-menu-drafts-header">
       Open a saved draft
-      <UButton color="neutral" variant="ghost" size="md" icon="i-lucide-x" class="float-right" @click="emit('close')" />
+      <UButton color="neutral" variant="ghost" size="md" icon="i-lucide-x" class="float-right" @click="closeMenu" />
     </div>
     <div class="create-menu-drafts-list">
       <UListbox v-model="selectedDraft" :search-term="searchTerm" filter :items="drafts" class="cursor-pointer" />
+    </div>
+    <div class="create-menu-drafts-manage float-right">
+      <UButton :disabled="selectedDraft === null" color="neutral" variant="outline" size="md" class="px-2 mr-2" icon="i-lucide-file-down" label="Open" @click="openDraft" />
+      <UButton :disabled="selectedDraft === null" color="neutral" variant="outline" size="md" class="px-2 ml-2" icon="i-lucide-trash" label="Delete" @click="deleteDraft" />
     </div>
   </div>
 </template>
